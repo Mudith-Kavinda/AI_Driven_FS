360ba8e549885324c61213e4de4667a9d6aab3d2
Mudith-Kavinda <kavindadesilva333@gmail.com>
2024-04-06 23:22:32 +0530

    Intialise the react project and add tailwind css and shadcn component library

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-400">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
