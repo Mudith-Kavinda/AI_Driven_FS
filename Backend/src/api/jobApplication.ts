380069389285e9b7268f669015a31ee703ca7c7c
Mudith-Kavinda <kavindadesilva333@gmail.com>
2024-05-01 10:22:03 +0530

    Add get job application by id function

--- Backend/src/api/jobApplication.ts
+++ Backend/src/api/jobApplication.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createJobApplicaton, getJobApplications } from "../application/features/jobApplication";
+import { createJobApplicaton, getJobApplications, getJobApplicationsById } from "../application/features/jobApplication";
 
 const jobApplicationRouter = express.Router();
 
 jobApplicationRouter.route("/").get(getJobApplications).post(createJobApplicaton);
+
+jobApplicationRouter.route("/:id").get(getJobApplicationsById);
 
 export default jobApplicationRouter;
